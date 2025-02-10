3f10192ce82055f1be126e199ca5bafb0b8164b9
naimulh247 <naimulh247@gmail.com>
2025-02-10 16:42:52 -0500

    feat: Add types and interfaces for database operations

---
+++ pkg/db/types.go
@@ -0,0 +1,29 @@
+package db
+
+import (
+	"time"
+
+	"github.com/naimulh247/logdb/internal/storage"
+)
+
+// TableOptions defines options for table creation
+type TableOptions struct {
+    TTL         time.Duration
+    PrimaryKeys []string
+}
+
+// Query defines query parameters
+type Query struct {
+    StartTime time.Time
+    EndTime   time.Time
+    Limit     int
+    Ascending bool
+}
+
+// RowIterator is an interface for iterating over query results
+type RowIterator interface {
+    Next() bool
+    Row() storage.Row
+    Error() error
+    Close() error
+}
